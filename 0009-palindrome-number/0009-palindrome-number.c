19abd1bf04e8ad3839a3b5d806a7720ad257513b
saurabhqr1 <saurabhsinghqr@gmail.com>
2026-02-13 23:56:03 +0530

    Time: 2 ms (44.74%), Space: 8.1 MB (45.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+     if (x < 0) return false;
+
+    int original = x;
+    long reversed = 0;   // safer
+
+    while (x != 0) {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+    return original == reversed;
+    
+}
