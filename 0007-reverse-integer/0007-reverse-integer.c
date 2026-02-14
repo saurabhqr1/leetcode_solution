d791a638f80b7dee1952af6e23ea354e16c6fff5
saurabhqr1 <saurabhsinghqr@gmail.com>
2026-02-14 19:41:03 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.48%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,17 @@
+int reverse(int x){
+int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // overflow check BEFORE update
+            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+}
