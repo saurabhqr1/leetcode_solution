0e995319b23ff461d6759ee9ec8e37c27ac71c83
Saurabh Singh <187245579+saurabhqr1@users.noreply.github.com>
2026-07-03 21:51:51 +0530

    Time: 3 ms (64.58%), Space: 47.5 MB (77.96%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        int n = nums.length;
+        int i = 0;
+        while(i<n){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]) swap(nums, i, correct);
+            else   i++;
+        }
+        for(i = 0 ; i<n ; i++){
+            if(nums[i] != i+1) return new int[]{nums[i], i+1};
+        }
+        return new int[]{-1,-1};
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
